4da6e79eabfdcafb9cceda2b655f5e5d6bf55398
23jy0105 <23jy0105@jec.ac.jp>
2024-10-23 16:27:25 +0900

    Update HelloWorld.java
    test

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,6 @@
 public class HelloWorld{
   public static void main{string[] args){
   System.out.println("HelloWorld");
-  System.out.println("HelloWorld");
-  System.out.println("HelloWorld");
-  System.out.println("HelloWorld");
-  System.out.println("HelloWorld");
-  System.out.println("HelloWorld");
-  System.out.println("HelloWorld");
+ 
     }
 }
