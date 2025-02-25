4627d7f8abe7bc681fc0019735934484f490e367
inoue <23jy0105@jec.ac.jp>
2025-02-25 15:32:03 +0900

    dao+class

--- my_java_project/src/main/java/model/Room.java
+++ my_java_project/src/main/java/model/Room.java
@@ -1,5 +1,87 @@
 package model;
 
-public class Room {
+import java.sql.Timestamp;
 
+public class Room {
+	private String roomNo;
+	private String typeNo;
+	private String roomState;
+	private String roomKey;
+	private Timestamp keyUpdateTime;
+	private Timestamp enterRoomTime;
+	private String siteManagementKey;
+	
+	public Room() {
+		
+	}
+	
+	public Room(String roomNo, String typeNo, String roomState, String roomKey, Timestamp keyUpdateTime,
+			Timestamp enterRoomTime, String siteManagementKey) {
+		this.roomNo = roomNo;
+		this.typeNo = typeNo;
+		this.roomState = roomState;
+		this.roomKey = roomKey;
+		this.keyUpdateTime = keyUpdateTime;
+		this.enterRoomTime = enterRoomTime;
+		this.siteManagementKey = siteManagementKey;
+	}
+
+	public String getRoomNo() {
+		return roomNo;
+	}
+
+	public void setRoomNo(String roomNo) {
+		this.roomNo = roomNo;
+	}
+
+	public String getTypeNo() {
+		return typeNo;
+	}
+
+	public void setTypeNo(String typeNo) {
+		this.typeNo = typeNo;
+	}
+
+	public String getRoomState() {
+		return roomState;
+	}
+
+	public void setRoomState(String roomState) {
+		this.roomState = roomState;
+	}
+
+	public String getRoomKey() {
+		return roomKey;
+	}
+
+	public void setRoomKey(String roomKey) {
+		this.roomKey = roomKey;
+	}
+
+	public Timestamp getKeyUpdateTime() {
+		return keyUpdateTime;
+	}
+
+	public void setKeyUpdateTime(Timestamp keyUpdateTime) {
+		this.keyUpdateTime = keyUpdateTime;
+	}
+
+	public Timestamp getEnterRoomTime() {
+		return enterRoomTime;
+	}
+
+	public void setEnterRoomTime(Timestamp enterRoomTime) {
+		this.enterRoomTime = enterRoomTime;
+	}
+
+	public String getSiteManagementKey() {
+		return siteManagementKey;
+	}
+
+	public void setSiteManagementKey(String siteManagementKey) {
+		this.siteManagementKey = siteManagementKey;
+	}
+	
+	
+	
 }
--- my_java_project/src/main/webapp/Daotset.jsp
+++ my_java_project/src/main/webapp/Daotset.jsp
@@ -1,31 +1,66 @@
-<%@page import="model.Meal"%>
-<%@page import="dao.MealDao"%>
+<%@page import="model.Room"%>
+<%@page import="model.RoomType"%>
+<%@page import="model.RoomRemainingCount"%>
+<%@page import="dao.RoomDao"%>
 <%@page import="java.util.ArrayList"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
     <%
-    MealDao dao  = new MealDao();
-ArrayList<Meal> mlist = dao.findAll();
+    RoomDao dao  = new RoomDao();
+ArrayList<Room> mlist = dao.findAllRoom();
+ArrayList<RoomType> m2list = dao.roomType();
+ArrayList<RoomRemainingCount> m3list = dao.findRoomRemaining();
 %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>Insert title here?</title>
+<h1>aaa</h1>
 </head>
 <body>
-<%
-	for (Meal m:mlist) {
-		%>
+<h1>aaa</h1>
+
+	<%
+	
+	for (Room m:mlist) {
+	%>
 		
-			<p><%=m.getMealNo()%></p>
-			<p><%=m.getMealName()%></p>
-			<p><%=m.getAllergen()%></p>
-			<p><%=m.getMealImage()%></p>
+			<label><%=m.getRoomNo()%></label>
+			<label><%=m.getTypeNo()%></label>
+			<label><%=m.getRoomState()%></label>
+			<label><%=m.getRoomKey()%></label>
+			<label><%=m.getEnterRoomTime()%></label>
+			<label><%=m.getKeyUpdateTime()%></label>
+			<p><%=m.getSiteManagementKey()%></p>
+			
+			
+			<%
+	
+	}for (RoomType m2:m2list) {
+	%>
+		
+			<label><%=m2.getRoomType()%></label>
+			<label><%=m2.getTypeName()%></label>
+			<label><%=m2.getMaxAccommodation()%></label>
+			<p><%=m2.getTotalRoom()%></p>
+			
+
+<%
+	
+	}for (RoomRemainingCount m3:m3list) {
+	%>
+		
+			<label><%=m3.getDate()%></label>
+			<label><%=m3.getRoomType()%></label>>
+			<p><%=m3.getReserved()%></p>
+			
+
+
 		
 		<%
 		}
-dao.connectionClose();
+		dao.connectionClose();
 		%>
 </body>
 </html>
