1b685a04dddb5de68a7c057fc32bb0f70cf6b26b
23jy0129 <23jy0129@jec.ac.jp>
2025-02-25 10:38:47 +0900

    add

---
+++ my_java_project/src/main/java/dao/PlanDao.java
@@ -0,0 +1,12 @@
+package dao;
+
+import javax.servlet.http.HttpServlet;
+
+import connection.DBConnection;
+
+public class PlanDao extends HttpServlet{
+	private DBConnection connection;
+	public PlanDao() {
+		
+	}
+}
