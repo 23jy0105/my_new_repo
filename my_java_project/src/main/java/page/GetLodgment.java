343689f3488e21f1b4b7e1d0e3fc7c37d564e5c9
inoue <23jy0105@jec.ac.jp>
2025-02-28 20:08:23 +0900

    add

--- my_java_project/src/main/java/page/GetLodgment.java
+++ my_java_project/src/main/java/page/GetLodgment.java
@@ -1,41 +1,30 @@
-package page;
-
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class GetLodgment
- */
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import dao.RoomDao;
+import model.Room;
+
 @WebServlet("/GetLodgment")
 public class GetLodgment extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public GetLodgment() {
-        super();
-        // TODO Auto-generated constructor stub
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 最新のデータを取得
+    	RoomDao dao = new RoomDao();
+        Room room = dao.findAllRoom();
+        
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString();
+
+        // クライアントにJSONを返す
+        response.setContentType("application/json");
+        response.getWriter().write(currentTime);
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
